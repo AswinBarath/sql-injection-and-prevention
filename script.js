548bc7cd2a03ceab53bf12b35c04ab96b85fa69b
AswinBarath <aswin2001barath@gmail.com>
2021-01-07 19:01:40 +0530

    Adding new features

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunction1() {
+    document.getElementById("uname").value = document.getElementById("uname1").value;
+    document.getElementById("pass").placeholder = document.getElementById("pass1").placeholder;
+}
+
+function myFunction2() {
+    document.getElementById("uname").value = document.getElementById("uname2").value;
+    document.getElementById("pass").placeholder = document.getElementById("pass2").placeholder;
+}
