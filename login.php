6cfc62043045d2bc39a4b3116bad116bea1feb01
AswinBarath <aswin2001barath@gmail.com>
2021-01-07 15:27:42 +0530

    Correction in code

--- login.php
+++ login.php
@@ -13,14 +13,8 @@ $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-
-// $uname = $_GET['uname'];
-// $pass = $_GET['pass'];
-// $servername = "localhost";
-// $username = 'root';
-// $password = '';
-// $conn = new mysqli($servername, $username, $password, 'testdb');
-
+$uname = $_GET['uname'];
+$pass = $_GET['pass'];
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
