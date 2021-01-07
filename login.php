b8e675209da14ee09f1d92268e431eeafb869667
AswinBarath <aswin2001barath@gmail.com>
2021-01-07 15:17:47 +0530

    Adding new files

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+// $uname = $_GET['uname'];
+// $pass = $_GET['pass'];
+// $servername = "localhost";
+// $username = 'root';
+// $password = '';
+// $conn = new mysqli($servername, $username, $password, 'testdb');
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM login_details WHERE username = '$uname' AND password = '$pass'";
+$result = mysqli_query($conn, $sql);
+$check = mysqli_fetch_array($result);
+
+if(isset($check)){
+	echo '<h1>Connection is successful</h1>';
+}
+else {
+	echo '<h1>Connection failed.</h1><p>Wrong user credentials</p>';
+}
+
+?>
